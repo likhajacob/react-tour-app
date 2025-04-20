e84541888a3b2cff66595f593decac3c707cf204
Likha Jacob <likha.jacob03@gmail.com>
2025-04-20 16:38:57 -0400

    fixed error of code issue in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Gallery from './Gallery';
+import Gallery from "./components/Gallery";
 import DestinationSelector from './DestinationSelector';
 import toursData from './toursData';
 
